aaa10cfe0b4101dabe71dcd96ebec0f24d3e7e92
iccrow <155780488+iccrow@users.noreply.github.com>
2026-04-27 19:04:51 -0400

    switched to 1.19.2 build

--- src/main/java/com/crow/mimicked/Mimicked.java
+++ src/main/java/com/crow/mimicked/Mimicked.java
@@ -144,7 +144,7 @@ public class Mimicked
                 player.getX() + radius, player.getY() + radius, player.getZ() + radius
         );
 
-        List<Entity> entities = player.level().getEntities(player, box, e -> !(e instanceof Player) && e instanceof LivingEntity);
+        List<Entity> entities = player.level.getEntities(player, box, e -> !(e instanceof Player) && e instanceof LivingEntity);
 
         if (entities.isEmpty()) return null;
 
